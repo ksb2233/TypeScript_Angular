3dc3b3e35aa5aaff2391bf32731cce9abc42cd73
2233ksb <2233ksb@naver.com>
2020-12-17 15:22:52 +0900

    1217

--- 3.Functions/this.ts
+++ 3.Functions/this.ts
@@ -57,4 +57,25 @@ let deck2: Deck = {
 let cardPicker2 = deck2.createCardPicker();
 let pickedCard2 = cardPicker2();
 
-alert("card: " + pickedCard.card + " of " + pickedCard2.suit);
+console.log("card: " + pickedCard2.card + " of " + pickedCard2.suit);
+
+
+/**
+ * this
+ * 콜백에서 this 매개변수 (this parameters in callbacks)
+ * - 나중에 호출할 콜백 함수를 라이브러리에 전달할 때 this 에 발생하는 오류를 막기위해 콜백 타입을 this 로 명시해주어야한다
+ * - this: void 는 this 타입을 요구하지 않는 함수가 될것으로 에상한다는 것을 의미
+ */
+interface UIElement {
+    addClickListener(onclick: (this: void, e: Event) => void): void;
+}
+
+class Handler {
+    info: string;
+    onClickGood(this: void, e: Event) {
+        // void 타입이기 때문에 this는 이곳에서 쓸 수 없습니다!
+        console.log('clicked!');
+    }
+}
+let h = new Handler();
+// uiElement.addClickListener(h.onClickGood);
--- 4.Literal_Type/Literal_Narrowing.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 리터럴 타입 좁히기 (Literal_Narrowing)
- */
-const helloWorld = "Hello World";
-
-let hiWorld = "Hi World";
